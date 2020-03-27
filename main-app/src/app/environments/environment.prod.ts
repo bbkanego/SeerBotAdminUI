3db7a954a02ecca3811922c5696082e7f7792a97
Bhushan Kane <bbkanego@gmail.com>
2020-03-26 20:18:32 -0400

    Update Deploy scripts for BotAdmin and SeerBot

--- main-app/src/app/environments/environment.prod.ts
+++ main-app/src/app/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const rootEnvironment = {
   production: true,
   envName: 'prod',
-  apiUrl: 'http://BizBotAdminELB-898893764.us-east-2.elb.amazonaws.com/botadmin'
+  apiUrl: 'http://BizBotAdminELB-283174543.us-east-2.elb.amazonaws.com/botadmin'
 };
