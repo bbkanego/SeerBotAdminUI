54d171d1793e81a38700af53a242c164aefb3108
bkane <bbkanego@gmail.com>
2020-12-11 22:39:15 -0500

    Fix small issues, improve help, fix issues with error handling

--- main-app/updateComponentLib.sh
+++ main-app/updateComponentLib.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-cd ~/Bhushan/code/angular/Angular-Library-With-NgPackagr
+cd ~/svn/code/angular/Angular-Library-With-NgPackagr
 echo '---------------------- Build the library'
 ng build seerlogics-ngui-components
 
 echo '---------------------- Create a TAR file'
 npm pack ./dist/seerlogics-ngui-components
 
-cd ~/Bhushan/code/angular/SeerlogicsBotAdminUI/main-app/
-cp ~/Bhushan/code/angular/Angular-Library-With-NgPackagr/seerlogics-ngui-components-1.0.0.tgz .
+cd ~/svn/code/angular/SeerlogicsBotAdminUI/main-app/
+cp ~/svn/code/angular/Angular-Library-With-NgPackagr/seerlogics-ngui-components-1.0.0.tgz .
 
 echo '---------------------- Install the library for testing'
 npm install ./seerlogics-ngui-components-1.0.0.tgz --save
--- main-app/updateComponentLibUbuntu.sh
+++ main-app/updateComponentLibUbuntu.sh
@@ -1,3 +1,14 @@
-#bin/bash
+#!/usr/bin/env bash
 
-./updateComponentLib.sh
+cd ~/Bhushan/code/angular/Angular-Library-With-NgPackagr
+echo '---------------------- Build the library'
+ng build seerlogics-ngui-components
+
+echo '---------------------- Create a TAR file'
+npm pack ./dist/seerlogics-ngui-components
+
+cd ~/Bhushan/code/angular/SeerlogicsBotAdminUI/main-app/
+cp ~/Bhushan/code/angular/Angular-Library-With-NgPackagr/seerlogics-ngui-components-1.0.0.tgz .
+
+echo '---------------------- Install the library for testing'
+npm install ./seerlogics-ngui-components-1.0.0.tgz --save
